03c6c1344c8d1efd824286fcaf6135e22bd8b05f
tvdboom <m.524687@gmail.com>
2026-02-11 10:40:18 +0100

    remove player's starting boost

--- src/core/player.rs
+++ src/core/player.rs
@@ -196,7 +196,7 @@ impl Player {
             strategy_timer: timer,
             queue: VecDeque::new(),
             queue_default: UnitName::default(),
-            boosts: vec![SelectedBoost::new(Boost::Respawn)],
+            boosts: vec![],
         }
     }
 
